00ab9efd4526a27898c3fa5b9642911633c2d6a2
mwaniasam <s.mwania@alueducation.com>
2024-10-30 09:15:21 +0300

    Client configuration file (w/ Puppet)

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,22 @@
-# Client configuration file with Puppet
-file_line { 'Turn_off_passwd_auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
+#!/usr/bin/env bash
+
+# make changes to config file using puppet
+
+file {'/etc/ssh/ssh_config':
+  ensure => 'present',
 }
 
-file_line { 'Declare_identity_file':
+file_line {'Turn off passwd auth':
+}
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
+
+file_line {'Use a Identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/config',
+  match   => '^IdentityFile',
+  ensure  => 'present',
+
 }
