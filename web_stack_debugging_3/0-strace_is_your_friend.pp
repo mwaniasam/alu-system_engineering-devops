5118b6aef2c0e44110731871d3ebc12b478c81ab
mwaniasam <s.mwania@alustudent.com>
2024-11-25 14:38:41 -0800

    Fix the phpp error

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # a puppet code that fixes a wordpress website a 5xx error200 ok
 #editing the mistyped .phpp to php in the /var/www/html/wp-settings.php file
 
-exec { 'fix-wordpess-server-error':
+exec { 'fix-wordpress-server-error':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/usr/bin/:bin/',
 }
