15790840a82defdcebd165cb95303119462ce4f1
mwaniasam <s.mwania@alustudent.com>
2024-11-25 15:20:16 -0800

    Sky is the limit, let's bring that limit higher(Increase nginx requests fromi5 to 4096)

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# a puppet code that increases the ULIMIT of nginx from 15 to 4096
+
+#to increase ULIMIT
+
+exec { 'increase ulimit':
+    command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
+}
+
+#restart nginx
+
+exec { 'restart-nginx':
+    command => '/usr/sbin/service nginx restart'
+}
