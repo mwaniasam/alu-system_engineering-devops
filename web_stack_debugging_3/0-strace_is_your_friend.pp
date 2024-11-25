39596c8d98826708892b620773a6fb2ceac5c4b6
mwaniasam <s.mwania@alustudent.com>
2024-11-25 14:08:19 -0800

    Added a comma on line six.

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 #editing the mistyped .phpp to php in the /var/www/html/wp-settings.php file
 
 exec { 'fix-wordpress-server-error':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/usr/bin/:bin/',
 }
