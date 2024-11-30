77746f70cd767d46ee289989d42e57643b3d9320
mwaniasam <s.mwania@alustudent.com>
2024-11-30 02:52:54 -0800

    The sky is not the limit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,8 @@
 # a puppet code that increases the ULIMIT of nginx from 15 to 4096
 
 #to increase ULIMIT
-exec { 'increase-ulimit':
-    command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
-}
 
-#restart nginx
-exec { 'restart-nginx':
-    command => '/usr/sbin/service nginx restart'
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
